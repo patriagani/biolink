4a9e576767c70544e402a365d4d2bab92001c4ae
patria gani <patria.gani@gmail.com>
2020-02-10 16:59:50 +0700

    first commit client

---
+++ client/src/components/LoginForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container>
+    <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="First name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            :counter="10"
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'LoginForm',
+
+    data: () => ({
+     
+    }),
+  }
+</script>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <v-content>
+    <LoginForm/>
+    </v-content>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LoginForm from '@/components/LoginForm.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    LoginForm
+  }
+}
+</script>
